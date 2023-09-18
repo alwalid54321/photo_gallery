991a202c978bbb1e8e27ac07cd631d0ebea3ac62
Alwalid Yasir <113239668+alwalid54321@users.noreply.github.com>
2023-09-19 00:24:18 +0400

    Add files via upload

---
+++ pg.js
@@ -0,0 +1,26 @@
+// Photo gallery 
+
+// Get all gallery items
+const galleryItems = document.querySelectorAll('.col-xl-3'); 
+
+// Add click event to each item
+galleryItems.forEach(item => {
+  item.addEventListener('click', e => {
+    // Add/remove active class
+    item.classList.toggle('active');
+  });
+});
+
+
+// Footer
+
+// Get footer container 
+const footer = document.createElement('footer');
+
+// Add content
+footer.innerHTML = `
+
+`;
+
+// Add to end of page
+document.body.appendChild(footer);
